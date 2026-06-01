06cad3f2e41d33c88d36ec7ec5d9cfea81a624a5
Javier <35864946+jmagit@users.noreply.github.com>
2026-06-01 14:59:56 +0200

    config

--- curso/src/app/app.ts
+++ curso/src/app/app.ts
@@ -8,5 +8,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('curso');
+  protected readonly title = signal('Curso de Angular');
+  p: number = 0
+
 }
