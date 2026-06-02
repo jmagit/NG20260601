7ff6d676e637af1c62709551e44a248b74fdce56
Javier <35864946+jmagit@users.noreply.github.com>
2026-06-02 09:32:09 +0200

    señales

--- curso/src/app/ejemplos/demos/demos.ts
+++ curso/src/app/ejemplos/demos/demos.ts
@@ -1,9 +1,29 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, effect, signal } from '@angular/core';
 
 @Component({
   selector: 'app-demos',
   imports: [],
   templateUrl: './demos.html',
   styleUrl: './demos.css',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Demos {}
+export class Demos {
+  readonly conSignal = signal(0)
+  readonly doble = computed(() => this.conSignal() * 2)
+  sinSignal = 0
+
+  constructor() {
+    setInterval(() => this.conSignal.update(value => value + 1), 2_000)
+    setInterval(() => this.sinSignal++, 1_000)
+    effect(() => {
+      console.log(`Contador: ${this.conSignal()}`)
+    })
+  }
+
+  addSignal() {
+    this.conSignal.update(value => value + 1)
+  }
+  addSinSignal() {
+    this.sinSignal++
+  }
+}
