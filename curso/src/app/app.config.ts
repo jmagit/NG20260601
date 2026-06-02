257f4b980a4ab61e831f7d8b0124499f4a04165b
Javier <35864946+jmagit@users.noreply.github.com>
2026-06-02 10:25:40 +0200

    LoggerService v.1

--- curso/src/app/app.config.ts
+++ curso/src/app/app.config.ts
@@ -2,10 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { LoggerService } from '@my/library';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideRouter(routes, withComponentInputBinding()),
+    LoggerService,
+
   ]
 };
--- curso/src/app/ejemplos/demos/demos.ts
+++ curso/src/app/ejemplos/demos/demos.ts
@@ -1,4 +1,6 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { ChangeDetectionStrategy, Component, computed, effect, signal } from '@angular/core';
+import { LoggerService } from '@my/library';
 
 @Component({
   selector: 'app-demos',
@@ -8,17 +10,26 @@ import { ChangeDetectionStrategy, Component, computed, effect, signal } from '@a
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Demos {
+
+  constructor(private log: LoggerService) {
+    log.error('esto es un error')
+    log.warn('esto es un warn')
+    log.info('esto es un info')
+    log.log('esto es un log')
+  }
+
+  // ejemplo de señales
   readonly conSignal = signal(0)
   readonly doble = computed(() => this.conSignal() * 2)
   sinSignal = 0
 
-  constructor() {
-    setInterval(() => this.conSignal.update(value => value + 1), 2_000)
-    setInterval(() => this.sinSignal++, 1_000)
-    effect(() => {
-      console.log(`Contador: ${this.conSignal()}`)
-    })
-  }
+  // constructor() {
+  //   setInterval(() => this.conSignal.update(value => value + 1), 2_000)
+  //   setInterval(() => this.sinSignal++, 1_000)
+  //   effect(() => {
+  //     console.log(`Contador: ${this.conSignal()}`)
+  //   })
+  // }
 
   addSignal() {
     this.conSignal.update(value => value + 1)
